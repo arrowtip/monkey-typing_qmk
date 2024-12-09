2a57c29344d64c677c057ea2387827872c162f6e
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-12-09 18:58:33 +0100

    add secomd layer for braces

--- v1/keymaps/arrowtip/keymap.c
+++ v1/keymaps/arrowtip/keymap.c
@@ -4,11 +4,13 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    BASE_LAYER,
+    BASE_LAYER = 0,
+    FN_LAYER,
 };
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [BASE_LAYER] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+    [FN_LAYER] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -30,7 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_PAUS, KC_LGUI, KC_ESC,  KC_SPC,  KC_LGUI,            KC_ENT,   KC_BSPC, KC_LCTL, KC_LALT, KC_RGUI
+                          KC_PAUS, KC_LGUI, KC_ESC,  KC_SPC,  MO(FN_LAYER),       KC_ENT,   KC_BSPC, KC_LCTL, KC_LALT, KC_RGUI
+    ),
+    [FN_LAYER] = LAYOUT_iso(
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                  KC_TRNS,    KC_TRNS,    KC_LBRC,    KC_RBRC,    KC_TRNS,    KC_TRNS,
+                                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,            KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
     )
 };
 
