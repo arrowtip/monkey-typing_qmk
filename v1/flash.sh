4e9e3ca9fdfb830487d871d8c00aff16cb6e8bcc
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-12-30 11:45:58 +0100

    add print key and volume control

---
+++ v1/flash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qmk flash -kb monkey_typing/v1 -km arrowtip
--- v1/keymaps/arrowtip/keymap.c
+++ v1/keymaps/arrowtip/keymap.c
@@ -16,7 +16,7 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [BASE_LAYER] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+    [BASE_LAYER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
     [FN_LAYER] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
 };
 
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_PAUS, KC_LCTL, KC_ESC,  KC_SPC,  MO(FN_LAYER),       KC_ENT,   KC_BSPC, KC_LGUI, KC_LALT, KC_RGUI
+                          KC_MPLY, KC_LCTL, KC_ESC,  KC_SPC,  MO(FN_LAYER),       KC_ENT,   KC_BSPC, KC_LGUI, KC_LALT, KC_RGUI
     ),
     [FN_LAYER] = LAYOUT_iso(
-        TRN,        TRN,        TRN,        TRN,        TRN,        KC_SLEP,                                      TRN,        TRN,        TRN,        TRN,        TRN,        KC_EQL,
+        TRN,        TRN,        TRN,        TRN,        TRN,        KC_SLEP,                                  KC_PSCR,    TRN,        TRN,        TRN,        TRN,        KC_EQL,
         TRN,        TRN,        TRN,        TRN,        TRN,        TRN,                                      TRN,        DE_UE,      TRN,        DE_OE,      TRN,        TRN,
         TRN,        DE_AE,      DE_SS,      TRN,        TRN,        TRN,                                      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    TRN,        TRN,
         TRN,        TRN,        TRN,        TRN,        TRN,        TRN,                                      TRN,        TRN,        KC_LBRC,    KC_RBRC,    TRN,        TRN,
